7f00c3c01e1ff933f0cd0043439bb5b5730b32cb
rafael <rafaelhss@gmail.com>
2017-06-08 14:19:27 -0300

    colocando printstacktrace

--- src/main/java/com/foodlog/FoodLogBotBatch.java
+++ src/main/java/com/foodlog/FoodLogBotBatch.java
@@ -43,6 +43,7 @@ public class FoodLogBotBatch implements CommandLineRunner {
 			}
 		} catch (Exception ex){
 			System.out.println("errroooooooooooooooooooooooooooooooooooooooooo: " + ex.getMessage());
+			ex.printStackTrace();
 		}
 
 
