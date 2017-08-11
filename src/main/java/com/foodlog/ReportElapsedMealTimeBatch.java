61b25b08ec7746eb8580d3a26a30904abe1bef30
rafael <rafaelhss@gmail.com>
2017-08-11 14:48:15 -0300

    Tentando ver o bug das mensagem de madruga

--- src/main/java/com/foodlog/ReportElapsedMealTimeBatch.java
+++ src/main/java/com/foodlog/ReportElapsedMealTimeBatch.java
@@ -2,6 +2,8 @@ package com.foodlog;
 
 import com.foodlog.meallog.MealLog;
 import com.foodlog.meallog.MealLogRepository;
+import com.foodlog.scheduledmeal.ScheduledMeal;
+import com.foodlog.scheduledmeal.ScheduledMealRepository;
 import com.foodlog.sender.Sender;
 import com.foodlog.sender.sentmessage.SentMessage;
 import com.foodlog.sender.sentmessage.SentMessageRepository;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalUnit;
 import java.util.Date;
@@ -22,11 +26,13 @@ public class ReportElapsedMealTimeBatch {
 
 
     public static final String MSGTYPE = "hour";
+
     @Autowired
     private MealLogRepository mealLogRepository;
 
     @Autowired
     SentMessageRepository sentMessageRepository;
+
 
     public void run(){
 
@@ -39,7 +45,9 @@ public class ReportElapsedMealTimeBatch {
         System.out.println(Instant.now());
 
         // ElapsedMealTime eh sempre um numero redondo. A segunda condição evita que envie mensagem duplicada.
-        if(elapsedMealTime >= 3 && sentMessageRepository.findBySentIdAndMessageType(elapsedMealTime, MSGTYPE) == null){
+        if( elapsedMealTime >= 3 &&
+            sentMessageRepository.findBySentIdAndMessageType(elapsedMealTime, MSGTYPE) == null &&
+            !isNowSleepTime(mealLog)){
             message = "Hora de comer! Sua ultima refeição ";
 
             if(mealLog.getScheduledMeal() != null){
@@ -70,4 +78,16 @@ public class ReportElapsedMealTimeBatch {
 
 
     }
+
+    //O horario limite para envio das mensagens de mais de X horas sem comer eh meia noite do ultimo dia que já teve Log
+    private boolean isNowSleepTime(MealLog mealLog) {
+        ZonedDateTime midnightOfLastMeal = mealLog.getMealDateTime().atZone(ZoneId.of("America/Sao_Paulo")).toLocalDate().atStartOfDay(ZoneId.of("America/Sao_Paulo")).plus(1, ChronoUnit.DAYS);
+
+        String mensagem = "Verificando se eh hora de dormir. midnightOfLastMeal:" + midnightOfLastMeal + "  ZonedDateTime.now(ZoneId.of(\"America/Sao_Paulo\")):" + ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")) + " isNowSleepTime? --> " + ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).isAfter(midnightOfLastMeal);
+        System.out.println(mensagem);
+        new Sender(BatchConfigs.BOT_ID).sendResponse(153350155, mensagem);
+
+        return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).isAfter(midnightOfLastMeal);
+    }
+
 }
